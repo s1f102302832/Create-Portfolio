2c616e1e3ea15781f77484038c96a50f8e8850cf
1F10230283 <s1f102302832@iniad.org>
2025-01-29 13:09:31 +0900

    jsファイル内のコード記述

--- js/main.js
+++ js/main.js
@@ -1,2 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // フローティングメニューをクリックすることで表示内容を変更する
+    const links = document.querySelectorAll(".menu-link");
+    const sections = document.querySelectorAll(".section");
 
-// フローティングメニューをクリックすることで表示内容を変更する
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault(); //デフォルトの挙動（ページ遷移を防ぐ）
+            const targetId = this.getAttribute("data-target") + "-section";
+
+            // 全てのセクションを非表示
+            sections.forEach(section => {
+                section.classList.add("hidden");
+            });
+
+            // クリックされたリンクに対応するセクションを表示
+            document.getElementById(targetId).classList.remove("hidden");
+        });
+    });
+});
